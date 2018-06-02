7ceaf772ba458278a60726ad5f3eb6957fafd075
Jonathan Gunn <gunnja87@gmail.com>
2018-06-02 07:33:16 +0000

    updating for heroku

--- server.js
+++ server.js
@@ -234,7 +234,7 @@ function twitterRequest() {
 
 // DB Functionality -------
 var pg = require('pg');
-var conString = `postgres://ubuntu:pgpass@localhost:5432/bookdb`;
+var conString = process.env.DATABASE_URL;
 var client = new pg.Client(conString);
 client.connect();
 
